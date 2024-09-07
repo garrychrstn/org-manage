81cc785b5b73361a89b5a0b7cc954f276c95a287
garrychrstn <garrychrstn@gmail.com>
2024-09-07 12:12:31 +0700

    initial

---
+++ database/migrations/2024_09_06_164506_create_prokers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prokers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('periode')->constrained();
+            $table->foreignId('divisi')->constrained();
+            $table->string('proker');
+            $table->string('pj');
+            $table->date('due_date');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prokers');
+    }
+};
